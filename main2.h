a92d96d0ac659e1854955f8dd2d37b657fd3e6f4
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-25 04:44:01 -0700

    feat: update header

--- main2.h
+++ main2.h
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 int _printf(const char *format, ...);
 int length(char *str);
 int printchar(va_list list);
